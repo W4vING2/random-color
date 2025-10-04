a39cd0aa046bee2afd9bae09078db64fec0415f6
Danila Vaganov <danilavaganov41@gmail.com>
2025-10-04 23:57:58 +0600

    svg bug fix

--- src/components/ButtonSave.jsx
+++ src/components/ButtonSave.jsx
@@ -26,7 +26,37 @@ export default function ButtonSave({ colorOne, colorTwo, type, color }) {
 				className='transition-all absolute top-1 right-1 bg-white/50 rounded-md text-black'
 				onClick={() => saveData(colorOne, colorTwo, type, color)}
 			>
-				<img src={`../../public/${url}.svg`} alt='Icon' />
+				{url === 'save' ? (
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'
+						fill='none'
+						stroke='currentColor'
+						strokeWidth='2'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					>
+						<path d='M15.2 3a2 2 0 0 1 1.4.6l3.8 3.8a2 2 0 0 1 .6 1.4V19a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z' />
+						<path d='M17 21v-7a1 1 0 0 0-1-1H8a1 1 0 0 0-1 1v7' />
+						<path d='M7 3v4a1 1 0 0 0 1 1h7' />
+					</svg>
+				) : (
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						width='24'
+						height='24'
+						viewBox='0 0 24 24'
+						fill='none'
+						stroke='white'
+						strokeWidth='2'
+						strokeLinecap='round'
+						strokeLinejoin='round'
+					>
+						<path d='M20 6 9 17l-5-5' />
+					</svg>
+				)}
 			</button>
 		</>
 	)
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,19 @@
 export default function Modal() {
 	return (
 		<div className='w-[400px] h-[50px] bg-green-500 flex items-center text-white fixed top-2 right-2 rounded-md px-5 gap-x-1 anim-translate'>
-			<img src='../../public/check.svg' alt='check' />
+			<svg
+				xmlns='http://www.w3.org/2000/svg'
+				width='24'
+				height='24'
+				viewBox='0 0 24 24'
+				fill='none'
+				stroke='white'
+				strokeWidth='2'
+				strokeLinecap='round'
+				strokeLinejoin='round'
+			>
+				<path d='M20 6 9 17l-5-5' />
+			</svg>
 			Success!
 		</div>
 	)
